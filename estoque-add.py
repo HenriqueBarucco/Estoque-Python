7d353a99f8804978642222d161195c25be42a5c2
Henrique  Sanches (GVD) <henrique.sanches@criar.com>
2023-04-12 15:56:01 -0300

    feat: contemplando mudança da api

--- estoque-add.py
+++ estoque-add.py
@@ -6,17 +6,29 @@ df = pd.read_excel("dados-estoque.xlsx", sheet_name="Plan1")
 
 for index, row in df.iterrows():
     item_name = row["A"]
-    description = row["C"]
+    model = row["C"]
     qnt = row["D"]
     price = row["E"]
 
-    if description != description:
-        description = ""
+    # Dividindo o texto em duas partes
+    values = item_name.split(" ", 1)
+    text = values[1] # "175/65/14 GOODYEAR KELLY EDGE TOURING 82T"
+
+    # Dividindo o texto restante em duas sub-partes
+    values2 = text.split(" ", 1)
+    measure = values2[0] # "175/65/14"
+    product = values2[1] # "GOODYEAR KELLY EDGE TOURING 82T"
+
+    print(measure) # "175/65/14"
+    print(product) # "GOODYEAR KELLY EDGE TOURING 82T"
+
+    if model != model:
+        model = ""
 
     unitPrice = price/qnt
-    teste = round(unitPrice, 2)
+    unitPriceFormatted = round(unitPrice, 2)
 
-    payload = {"name": item_name, "description": description, "available": 1000, "price": teste}
+    payload = {"name": product, "model": model, "measure": measure, "available": 1000, "price": unitPriceFormatted}
 
     response = requests.post("https://estoque-api.henriquebarucco.com.br/products", json=payload)
 

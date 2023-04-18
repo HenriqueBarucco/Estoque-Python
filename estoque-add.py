dc5e1e0be4f8f1dc4dd509b6da58aface710e278
Henrique  Sanches (GVD) <henrique.sanches@criar.com>
2023-04-18 14:01:18 -0300

    feat: alteração para o novo negocio

--- estoque-add.py
+++ estoque-add.py
@@ -2,20 +2,21 @@
 import pandas as pd
 import requests
 
-df = pd.read_excel("dados-estoque.xlsx", sheet_name="Plan1")
+df = pd.read_excel("2022.xlsx", sheet_name="Plan1")
 
 for index, row in df.iterrows():
-    item_name = row["A"]
+    item_name = str(row["A"])
     model = row["C"]
     qnt = row["D"]
     price = row["E"]
 
     # Dividindo o texto em duas partes
     values = item_name.split(" ", 1)
-    text = values[1] # "175/65/14 GOODYEAR KELLY EDGE TOURING 82T"
+    print(values[1])
+    #text = values[1] # "175/65/14 GOODYEAR KELLY EDGE TOURING 82T"
 
     # Dividindo o texto restante em duas sub-partes
-    values2 = text.split(" ", 1)
+    values2 = values[1].split(" ", 1)
     measure = values2[0] # "175/65/14"
     product = values2[1] # "GOODYEAR KELLY EDGE TOURING 82T"
 
@@ -30,6 +31,15 @@ for index, row in df.iterrows():
 
     payload = {"name": product, "model": model, "measure": measure, "available": 1000, "price": unitPriceFormatted}
 
-    response = requests.post("https://estoque-api.henriquebarucco.com.br/products", json=payload)
+    try:
+        response = requests.post("https://estoque-api.henriquebarucco.com.br/products", json=payload)
+        print(response.status_code)
+        response.raise_for_status()
 
-    print(f"Status code: {response.status_code}")
+        data = response.json()
+
+        payload = {"productId": data.get("id"), "quantity": qnt}
+        response = requests.post("https://estoque-api.henriquebarucco.com.br/sales", json=payload)
+        print(response.status_code)
+    except:
+        print("Produto já no estoque")
